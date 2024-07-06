726bda081c41b9c621e15f0810653b62f43953b8
Cristian <cristiancastrodevs@gmail.com>
2024-07-05 20:13:04 -0400

    teste pagina inicial whatsapp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,169 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { FaWhatsapp } from 'react-icons/fa';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import Image from 'next/image';
 
 export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [verifying, setVerifying] = useState(false);
+  const [qrCode, setQrCode] = useState('');
+  const [sessionId, setSessionId] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [timeLeft, setTimeLeft] = useState(50);
+
+  useEffect(() => {
+    let timer;
+    if (qrCode) {
+      timer = setInterval(() => {
+        setTimeLeft((prev) => {
+          if (prev <= 1) {
+            clearInterval(timer);
+            window.location.reload();
+            return 50;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [qrCode]);
+
+  const fetchQrCode = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('http://127.0.0.1:3002/api/whatsapp/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ phoneNumber }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.qrCode && data.sessionId) {
+        setQrCode(data.qrCode);
+        setSessionId(data.sessionId);
+      } else {
+        throw new Error('Invalid response structure');
+      }
+
+    } catch (error) {
+      console.error('Error fetching the QR Code:', error);
+      alert('Erro ao obter o QR Code. Por favor, tente novamente.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    value = value.replace(/\D/g, ''); // Remove non-numeric characters
+    if (value.length <= 13) {
+      setPhoneNumber(value);
+    }
+  };
+
+  const handleVerify = async () => {
+    setVerifying(true);
+    try {
+      const response = await fetch('http://127.0.0.1:3002/api/whatsapp/verify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ phoneNumber, sessionId }),
+      });
+      const data = await response.json();
+
+      setVerifying(false);
+
+      if (response.ok) {
+        localStorage.setItem('jwtToken', data.token);
+        alert('Redirecionar para dashboard');
+        console.log(data.token);
+        //router.push('/dashboard');
+      } else {
+        alert(data.message || 'Erro na verificação');
+      }
+    } catch (error) {
+      console.error('Error verifying the QR Code:', error);
+      alert('Erro ao tentar verificar o QR Code. Tente novamente.');
+      setVerifying(false);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg flex max-w-4xl w-full text-white">
+        <div className="flex flex-col justify-center items-center w-1/2 border-r border-gray-700 p-4">
+          <FaWhatsapp className="text-green-500 mb-4" size={60} />
+          <h1 className="text-3xl font-semibold mb-4">Bem-vindo!</h1>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            placeholder="Exemplo: 554599999999"
+            className="mb-4 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 w-full text-black"
+          />
+          <button
+            onClick={fetchQrCode}
+            disabled={phoneNumber.length !== 13 || loading}
+            className={`px-6 py-2 rounded-lg text-white w-full flex items-center justify-center ${loading ? 'bg-gray-400' : 'bg-green-500 hover:bg-green-600'}`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {loading ? <AiOutlineLoading3Quarters className="animate-spin" size={24} /> : 'Obter QR Code'}
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 p-4">
+          {loading && (
+            <div className="flex flex-col items-center justify-center mt-4">
+              <AiOutlineLoading3Quarters className="animate-spin text-green-500" size={40} />
+              <h1 className="text-lg font-semibold mt-2">Aguardando QR Code...</h1>
+            </div>
+          )}
+          {qrCode ? (
+            <div className="flex flex-col items-center">
+              <Image src={qrCode} alt="QR Code" width={256} height={256} />
+              <p className="mt-2 text-gray-300">O QR Code deve ser lido pelo WhatsApp, não pela câmera normal.</p>
+              <p className="mt-2 text-red-500">O QR Code expira em {timeLeft} segundos</p>
+              <button
+                onClick={handleVerify}
+                disabled={verifying}
+                className="mt-4 px-6 py-2 rounded-lg text-white bg-green-500 hover:bg-green-600 flex items-center justify-center"
+              >
+                {verifying ? <AiOutlineLoading3Quarters className="animate-spin" size={24} /> : 'Já li o QR Code'}
+              </button>
+            </div>
+          ) : (
+            <ul className="mt-4 text-left text-gray-300">
+              <li className="mb-2">1. Abra o WhatsApp no seu telefone</li>
+              <li className="mb-2">2. Toque em Menu ou Configurações e selecione WhatsApp Web</li>
+              <li className="mb-2">3. Aponte seu telefone para esta tela para capturar o código</li>
+            </ul>
+          )}
         </div>
       </div>
+      <style jsx>{`
+        .animate-spin {
+          animation: spin 1s linear infinite;
+        }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
   );
 }
